984c3bf68f9237e8e3bd1855dcd3f1b9cce27683
yuuya uezato <yuuya_uezato@dwango.co.jp>
2018-11-14 16:27:04 +0900

    allow the  argument

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,21 @@ extern crate proc_macro;
 extern crate syn;
 #[macro_use]
 extern crate quote;
+extern crate regex;
+use regex::Regex;
 
 use proc_macro::TokenStream;
 
 #[cfg_attr(feature = "cargo-clippy", allow(needless_pass_by_value))]
 #[proc_macro_attribute]
-pub fn trace_info(_args: TokenStream, input: TokenStream) -> TokenStream {
+pub fn trace_info(args: TokenStream, input: TokenStream) -> TokenStream {
+    let args_str = args.to_string();
+    let re = Regex::new(r#"^comment\s*=\s*"(.*?)"$"#).unwrap();
+    let mut comment: Option<&str> = None;
+    if let Some(cap) = re.captures(&args_str) {
+        comment = Some(cap.get(1).unwrap().as_str());
+    }
+
     let parsed: syn::ItemFn = syn::parse2(input.clone().into()).unwrap();
     let block = parsed.block;
     let vis = parsed.vis;
@@ -17,20 +26,43 @@ pub fn trace_info(_args: TokenStream, input: TokenStream) -> TokenStream {
     let inputs = decl.clone().inputs;
     let output = decl.clone().output;
 
-    let expanded = quote! {
-        #vis fn #ident(#inputs) #output {
-            let dir = env!("CARGO_MANIFEST_DIR");
-            let mut file_name = String::from(file!());
-            if !file_name.starts_with("/") {
-                file_name = format!("{}/{}", dir, file_name);
+    let expanded = if let Some(comment) = comment {
+        quote! {
+            #vis fn #ident(#inputs) #output {
+                let dir = env!("CARGO_MANIFEST_DIR");
+                let mut file_name = String::from(file!());
+                if !file_name.starts_with("/") {
+                    file_name = format!("{}/{}", dir, file_name);
+                }
+
+                println!("入==> `{}` [ {}:{} ]", stringify!(#ident), file_name, line!()+1);
+                println!("      {}", #comment);
+                let inner = ||{
+                    #block
+                };
+                let result = inner();
+                    println!("<==出 `{}` [ {}:{} ]", stringify!(#ident), file_name, line!()+1);
+                return result;
             }
-            println!("入==>  `{}` [ {}:{} ]", stringify!(#ident), file_name, line!()+1);
-            let inner = ||{
-                #block
-            };
-            let result = inner();
-            println!("<==出  `{}` [ {}:{} ]", stringify!(#ident), file_name, line!()+1);
-            return result;
+        }
+    } else {
+        quote! {
+            #vis fn #ident(#inputs) #output {
+                let dir = env!("CARGO_MANIFEST_DIR");
+                let mut file_name = String::from(file!());
+                if !file_name.starts_with("/") {
+                    file_name = format!("{}/{}", dir, file_name);
+                }
+
+                println!("入==> `{}` [ {}:{} ]", stringify!(#ident), file_name, line!()+1);
+
+                let inner = ||{
+                    #block
+                };
+                let result = inner();
+                println!("<==出 `{}` [ {}:{} ]", stringify!(#ident), file_name, line!()+1);
+                return result;
+            }
         }
     };
 
